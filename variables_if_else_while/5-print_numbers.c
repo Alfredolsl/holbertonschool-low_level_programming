8e77d1f9a2174892521e7f2442d6dfd20f863e09
Alfredolsl <alfredo.445566@gmail.com>
2023-10-18 12:24:35 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - No input and must return int
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
