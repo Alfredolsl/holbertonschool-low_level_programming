eb8cb727f178bf197960ec18339c4dc14992121c
Alfredolsl <alfredo.445566@gmail.com>
2023-10-17 18:00:18 -0700

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,16 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
  * main - no input and must return int
- * 
+ *
  * N will be a random number and the last digit (n % 10)
  * will determine if its 0, greater than 5, or else, less than 6 and not 0
- * 
  * Return: always 0
  */
+
 int main(void)
 {
 	int n;
