55709db55c1b8d39bc14ef1d11de4f46ec85bc70
Alfredolsl <alfredo.445566@gmail.com>
2023-10-18 21:16:06 -0700

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints "_putchar"
+ * main - Prints "_putchar" using command _putchar
  *
  * Return: always 0
  */
 
 int main(void)
 {
-	puts("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	return (0);
 }
