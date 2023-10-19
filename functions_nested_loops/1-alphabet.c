d67baf89ac685a328c98c7578d9958ddee4bde12
Alfredolsl <alfredo.445566@gmail.com>
2023-10-18 21:54:14 -0700

    Alphabet with _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main - Prints alphabet using for loop with _puthcar and a newline
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+	}
+	return (0);
+}
