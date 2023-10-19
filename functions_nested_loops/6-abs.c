a138382769381b73bd111d232d86e4ff7a245d3b
Alfredolsl <alfredo.445566@gmail.com>
2023-10-18 23:02:10 -0700

    Absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <'main.h'>
+
+/**
+ *_abs - Returns absolute value of an integer
+ *@int: int to check
+ *Return: Absolute int
+ */
+
+int _abs(int);
+{
+	if (int < 0)
+	{
+		return (int * -1);
+	}
+	else
+	{
+		return (int);
+	}
+}
