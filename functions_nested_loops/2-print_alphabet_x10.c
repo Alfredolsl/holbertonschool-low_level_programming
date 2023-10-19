97075be1f9d44bec4e37d580dc3c2ae14873cb35
Alfredolsl <alfredo.445566@gmail.com>
2023-10-18 22:17:46 -0700

    10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- *main - Prints alphabet 10 times with nest loops
+ *print_alphabet_x10 - Prints alphabet 10 times with nest loops
  *Return: None
  */
 
 void print_alphabet_x10(void)
 {
-	char c = 'a';
 	int i = 0;
 
 	while (i <= 9)
 	{
+		char c;
+
 		while (c = 'a' <= 'z')
 		{
 			_putchar(c);
