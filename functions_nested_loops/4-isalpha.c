19adf874283a7763f0aa2654a8d0033e4be2783e
Alfredolsl <alfredo.445566@gmail.com>
2023-10-18 22:40:15 -0700

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if input is a character
+ * @c: char to check
+ * Return: 1 if c is a letter, else 0
+ */
+
+int _isalpha(int c);
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
