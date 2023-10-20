4b5a281631d37732c5c2cfc65ad4d0cb62ddcda3
Alfredolsl <alfredo.445566@gmail.com>
2023-10-19 20:37:57 -0700

    n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 
 void print_to_98(int n)
 {
-	while (n != 98 || n == 98)
+	while (n != 98)
 	{
 		if (n < 98)
 		{
@@ -25,4 +25,5 @@ void print_to_98(int n)
 		else
 			printf("%d", n);
 	}
+	printf("%d", n);
 }
