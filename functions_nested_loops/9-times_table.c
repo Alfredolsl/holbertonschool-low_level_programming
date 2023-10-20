f10e30a135e92da377feeaa0303af1700307d2c6
Alfredolsl <alfredo.445566@gmail.com>
2023-10-19 20:00:53 -0700

    Times Table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,22 +16,20 @@ void times_table(void)
 
 		for (j = 0; j <= 9; j++)
 		{
-			if ((j != 0) && (j != 9))
-				_putchar(',');
-
 			if (i * j >= 10)
 			{
+				_putchar(' ');
 				_putchar(i * j / 10 + '0');
 				_putchar(i * j % 10 + '0');
-				_putchar(' ');
 			}
 			else
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(i * j + '0');
-				_putchar(' ');
-				_putchar(' ');
 			}
 		}
+				_putchar(',');
 		_putchar('\n');
 		i++;
 	}
