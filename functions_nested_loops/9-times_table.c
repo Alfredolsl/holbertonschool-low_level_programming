a6ad4656d0d18be16d064f363ec4a9a20871f8ab
Alfredolsl <alfredo.445566@gmail.com>
2023-10-19 19:42:46 -0700

    Times Table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * time_table - prints the 9 times table starting with 0
+ *
+ * Return: Void
+ */
+
+void time_table(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int j = 0;
+
+		for (j; j <= 9; j++)
+			_putchar(i * j + '0');
+		i++;
+	}
+}
