b966ce5a58e1eb69600773b36ead208855353bf3
Alfredolsl <alfredo.445566@gmail.com>
2023-10-19 20:13:20 -0700

    a + v

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns result
+ * @i: first int
+ * @j: second int
+ * Return: Fist int + Second int
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
