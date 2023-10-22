6255b6e8de5c33d99d358b00010bc5d2edca52d9
Alfredolsl <alfredo.445566@gmail.com>
2023-10-22 12:56:06 -0700

    Largest prime factor of a large number

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - prints largest prime factor of the number 612852475143
+ *
+ *Return: void
+ */
+
+void main(void)
+{
+	int n = 612852475143;
+	int i = 2;
+
+	int div n / i;
+
+	while (div != 1)
+	{
+		if (isdigit(div))
+			n = div;
+	}
+	printf("%d\n", n);
+}
