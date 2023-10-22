8d21153b294dbfd0a8cb4dd2928c7ea2946cc456
Alfredolsl <alfredo.445566@gmail.com>
2023-10-22 13:00:10 -0700

    Largest prime factor of a large number

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -11,11 +11,11 @@ int main(void)
 	long int n = 612852475143;
 	int i = 2;
 
-	int div n / i;
+	int div (n / i);
 
 	while (div != 1)
 	{
-		if (isdigit(div))
+		if (isdigit(div) == 1)
 			n = div;
 	}
 	printf("%d\n", n);
