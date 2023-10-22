e7ff181452d16a2498a2e26f0eb47dc1784c9dec
Alfredolsl <alfredo.445566@gmail.com>
2023-10-22 00:09:24 -0700

    classic Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - print numbers 1 to 100, multiples of 3 print Fizz, and multiples
+ * of 5, print Buzz, if numbers are multiple of both 3 and 5, print FizzBuzz.
+ *
+ * Return: void
+ */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 0; i == 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			puts("FizzBuzz ");
+		else if (i % 3 == 0)
+			puts("Fizz ");
+		else if (i % 5 == 0)
+			puts("Buzz ");
+		else
+			printf("%d ", i);
+	}
+}
