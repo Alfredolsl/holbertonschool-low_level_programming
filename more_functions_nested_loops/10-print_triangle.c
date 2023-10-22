99eadae571436b07433ea5290cf5555f6e375795
Alfredolsl <alfredo.445566@gmail.com>
2023-10-22 11:23:54 -0700

    Triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_triangle - prints a triangle depending on int size
+ *@size: size of triangle
+ *Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i;
+
+		for (i = size; i != 0; i--)
+		{
+			int j = 0;
+
+			while (j != i)
+			{
+				if (j < i)
+					_putchar(' ');
+				else if (j > i)
+					_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+		}
+	}
+}
