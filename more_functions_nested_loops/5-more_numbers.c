a9b3e91a71fc418a4c870e9eeecb0b37f2272b1f
Alfredolsl <alfredo.445566@gmail.com>
2023-10-21 22:20:00 -0700

    prints numbers from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,13 @@ void more_numbers(void)
 	int i, j;
 
 	for (i = 0; i <= 10; i++)
+	{
+		_putchar(i + '0');
 		for (j = 0; j <= 15; j++)
 		{
-			_putchar(j + '0');
 			if (i == 10)
 				_putchar(j / 10 + '0');
 		}
-	_putchar('\n');
+		_putchar('\n')
+	}
 }
