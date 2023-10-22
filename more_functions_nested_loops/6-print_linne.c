f657164ff5cae4f4ec7f025f544b1294b78f6e32
Alfredolsl <alfredo.445566@gmail.com>
2023-10-21 22:44:56 -0700

    print a straight line according to n

---
+++ more_functions_nested_loops/6-print_linne.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_line - print a straight line
+ *@n: length of line
+ *Return: void
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > i)
+	{
+		while (i <= n)
+			_putchar('_');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
