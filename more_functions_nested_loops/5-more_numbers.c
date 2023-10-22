57bd49818d3d6ceafb2ad5c21f2e8898713fbe6d
Alfredolsl <alfredo.445566@gmail.com>
2023-10-21 22:14:34 -0700

    prints numbers from 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints numbers from 0 to 14, followed by a newline
+ *
+ *Return: void
+ */
+
+void more_numbers(void);
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+		for (j = 0; j <= 15; j++)
+		{
+			_putchar(i + '0');
+			if (j <= 10)
+				_putchar(j / 10 + '0')
+		}
+	_putchar('\n')
+}
