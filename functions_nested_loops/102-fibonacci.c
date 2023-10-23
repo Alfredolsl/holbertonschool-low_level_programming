25881aea51a5aec1c1d869ab5d621e2a0f6d59f7
Alfredolsl <alfredo.445566@gmail.com>
2023-10-22 23:23:36 -0700

    print an integer

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int f;
+	int j = 0;
+
+	for (f = 1; f != 50; f++)
+	{
+		j += f
+		printf("%d, ", j);
+	}
+	putchar('\n');
+	return (0);
+}
