17327c04eca0b72b296b0c93f1cd67b0f4d960dd
Alfredolsl <alfredo.445566@gmail.com>
2023-11-04 12:22:16 -0700

    prints string in reverse

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,16 @@
 void rev_string(char *s)
 {
 	char str;
-	int i, j, k;
+	int i, j;
 
 	i = 0;
 
 	while (s[i] != '\0')
 		i++;
 
-	k = i - 1;
-
 	for (j = i - 1; j <= 0; j--)
 	{
 		str = s[j];
-		s[j] = s[k];
-		s[k--] = str;
+		s[--j] = str[j];
 	}
 }
