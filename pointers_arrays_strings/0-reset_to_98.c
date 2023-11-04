fea6964b0a77df8687ba36b201e9ca7dfe9c8b1a
Alfredolsl <alfredo.445566@gmail.com>
2023-11-03 18:27:49 -0700

    98 batter st.

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *reset_to_98 - takes a pointer to an int as parameter and updates the value it to points to to 98
+ *reset_to_98 - updates value of pointer to to 98
  *@n: integer to be updated via its address
  *Return: void
  */
