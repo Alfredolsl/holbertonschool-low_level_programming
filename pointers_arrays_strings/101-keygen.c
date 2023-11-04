04f04966221d70c459927f1d43930178c1321b08
Alfredolsl <alfredo.445566@gmail.com>
2023-11-04 16:23:25 -0700

    Don't hate the hacker, hate the code

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates acceptable random
+ * password for 101-crackme
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int pass[100]; /* array storing 100 integers */
+	int i, n, sum;
+
+	sum = 0;
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
