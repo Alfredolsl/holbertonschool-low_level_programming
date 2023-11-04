497967ef963bc870dac798deccc39f9d17329cec
Alfredolsl <alfredo.445566@gmail.com>
2023-11-04 15:49:15 -0700

    convert string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -23,27 +23,21 @@ int _atoi(char *s)
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sign--;
+			sign = -1;
 		else if (s[i] == '+')
-			sign++;
+			sign = 1;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			n = n * 10 + digit;
-			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0;
 		}
 		i++;
 	}
 	if (f == 0)
 		return (0);
 
-	if (sign > 0)
-		sign = 1;
-
 	return (n * sign);
-
 }
