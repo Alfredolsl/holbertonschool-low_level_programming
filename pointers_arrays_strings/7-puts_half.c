7fb6619d61411d8821a6ee3bb3fb7379dfd51255
Alfredolsl <alfredo.445566@gmail.com>
2023-11-04 13:31:09 -0700

    prints half of a string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,8 +14,16 @@ void puts_half(char *str)
 	while (str[len] != '\0')
 		len++;
 
-	for (i = len / 2; str[i] != '\0'; i++)
-		_putchar(str[i]);
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (len / 2) + 1; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
 
 	_putchar('\n');
 }
