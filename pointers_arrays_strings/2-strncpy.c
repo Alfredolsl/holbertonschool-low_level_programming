f2afa6cf8365895e42337cd7ef65174b1b9cf316
Alfredolsl <alfredo.445566@gmail.com>
2023-11-04 22:20:58 -0700

    copies a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: sourced string
+ * @n: number of bytes of string to copy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len, i;
+
+	len = 0; 
+	while (src[len] != '\0')
+		len++;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return(dest)
+}
