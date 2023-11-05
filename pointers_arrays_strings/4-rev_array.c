107e84a89985fdfcb0e274811446e49a9c1e9c9a
Alfredolsl <alfredo.445566@gmail.com>
2023-11-05 09:29:55 -0800

    reverese the conent of an array of integers

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the conent of an array of integers
+ * @a: array of integers
+ * @n: the number of elements to swap
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j;
+
+	j = 0
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		a[j++] = a[i];
+	}
+}
