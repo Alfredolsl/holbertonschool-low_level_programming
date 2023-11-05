4a43fd00a5ad187cfe8dfbb23af3faafc020a7e5
Alfredolsl <alfredo.445566@gmail.com>
2023-11-05 12:39:39 -0800

    prints an integer

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_number - prints an integer
+ *@n: integer to print
+ *Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	else
+		i = n;
+
+	while (i != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
