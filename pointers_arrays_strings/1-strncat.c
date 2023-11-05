3d939634086fb7e91a06ed1582dcf8d2688decb3
Alfredolsl <alfredo.445566@gmail.com>
2023-11-04 22:11:19 -0700

    concatenates two strings, with a n byte limit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,17 +10,17 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len, bytes, i;
+	int len, i;
 
 	while (dest[len] != '\0')
 	{
 		len++;
 	}
 
-	for (i = 0; src[!] != '\0' && i < n; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[len] = src[i];
-		len++
+		len++;
 	}
 
 	return (dest);
