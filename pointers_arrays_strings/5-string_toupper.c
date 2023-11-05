7bdc27184d7e5ee50f600a52bc9ccd324d04b276
Alfredolsl <alfredo.445566@gmail.com>
2023-11-05 10:01:00 -0800

    changes lowercase letters of a string to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters of a str to uppercase
+ * @*: string to modify
+ * Return: converted string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+
+	return (*s);
+}
