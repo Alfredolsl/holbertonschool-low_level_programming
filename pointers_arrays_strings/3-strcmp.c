6a52a21d5ebfebad3fb935a963c5f463bb325899
Alfredolsl <alfredo.445566@gmail.com>
2023-11-04 22:52:08 -0700

    compares two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; *s1[i] != '\0' || *s2[i] != '\0'; i++)
 	{
 		s1++;
 		s2++;
 	}
 
-	return (s1 - s2);
+	return (*s1 - *s2);
 }
