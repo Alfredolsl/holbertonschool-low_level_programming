444912d26c5214da07ccb13852436e4c39d638d5
Alfredolsl <alfredo.445566@gmail.com>
2023-11-07 18:38:02 -0800

    gets the length of a prefix substring

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,8 +2,8 @@
 
 /**
  * _strspn - gets the length of a prefix substring
- * @s - string to evaluate
- * @accept - string containing the list of characters to match from  s
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match from  s
  *
  * Return: the number of bytes in the initial segment
  * of s which consist only of bytes from accept
@@ -11,19 +11,23 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, k;
+	int i, j, k, flag;
 
 	i = 0;
 
 	for (j = 0; s[j] != '\0'; j++)
 	{
+		flag = 0;
 		for (k = 0; accept[k] != '\0'; k++)
 		{
 			if (s[j] == accept[k])
 			{
+				flag = 1;
 				i++;
 			}
 		}
+		if (flag == 1)
+			return (i)
 	}
-	return (i);
+	return (0);
 }
