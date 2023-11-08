fd34776000c93b286cda6b26461066554b3680ab
Alfredolsl <alfredo.445566@gmail.com>
2023-11-08 15:21:31 -0800

    diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,21 +10,15 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, ldiag, rdiag;
+	int i, ldiag, rdiag;
 
 	ldiag = 0;
 	rdiag = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				ldiag += a[(j * i) * size];
-				rdiag += a[((j * i) * size) - 1];
-			}
-		}
+		ldiag += a[*(a * size)];
+		rdiag += a[*(a * size) - 1];
 	}
 	printf("Sum of left diagonal: %d\n", ldiag);
 	printf("Sum of right diagonal: %d\n", rdiag);
