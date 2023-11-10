fa7f709a47f30534da6dc74a9e2b76819926c7b1
Alfredolsl <alfredo.445566@gmail.com>
2023-11-09 18:13:03 -0800

    diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,20 +10,15 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, ldiag, rdiag;
+	int i, ldiag, rdiag;
 
-	j = 0;
 	ldiag = 0;
 	rdiag = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		if (i == j)
-		{
-			ldiag += (*a * i) + i;
-			rdiag += ((*a * i) + i) - (i + 1);
-		}
-		j++;
+		ldiag += (*a * i) + i;
+		rdiag += ((*a * i) + i) - (i + 1);	
 	}
 	printf("%d, %d\n", ldiag, rdiag);
 }
