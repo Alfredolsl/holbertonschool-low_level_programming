990f170c784ea5c61f80a39c888a07381bc6d312
Alfredolsl <alfredo.445566@gmail.com>
2023-11-11 11:56:55 -0800

    sqrt using recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,16 +7,26 @@
  * -1 if square root isn't a natural integer
  */
 
-int _sqrt_recursion(int n, int guess)
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (guess * guess == n)
-	{
-		return (guess);
-	}
 	else
-	{
-		return (_sqrt_recursion(n, guess + 1));
-	}
+		return (_sqrt(n, 0));
+}
+
+/**
+ * _sqrt - function that calculates square root of n
+ * @n: natural number to be calculated
+ * guess: test number
+ * Return: guess if guess * guess is equal to n,
+ * else call itself and incrementing guess by 1
+ */
+
+int _sqrt(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+	else
+		return (_sqrt(n, guess + 1));
 }
