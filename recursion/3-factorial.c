2ef0ae554d6944e608a0f0cd2f20c17a6a036676
Alfredolsl <alfredo.445566@gmail.com>
2023-11-10 16:59:00 -0800

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,12 +9,12 @@
 int factorial(int n)
 {
 	/* base case */
-	if (n == 1)
+	if (n == 0)
 		return (0);
 	else if (n < 0)
 		return (-1);
 	else
 	{
-		return (n * factorial (n - 1));
+		return (n * factorial(n - 1));
 	}
 }
