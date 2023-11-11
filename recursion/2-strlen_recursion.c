66d8c43ec2e2970b594121a4a200a8ad12a0de6a
Alfredolsl <alfredo.445566@gmail.com>
2023-11-10 16:51:20 -0800

    return srtlen using recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,11 +3,12 @@
 /**
  * _strlen_recursion - returns length of a string using recursion
  * @s: string to return length from
+ * Return: integer with length of string
  */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s == '\0')
 		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
