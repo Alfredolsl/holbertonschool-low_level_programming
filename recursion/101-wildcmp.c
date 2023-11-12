043b04922866c12e4f1fdffe6e92876ee17faa42
Alfredolsl <alfredo.445566@gmail.com>
2023-11-12 11:09:18 -0800

    compares two strings if identical or not

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -9,12 +9,12 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 	else if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
 	else if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	else
 		return (0);
 }
