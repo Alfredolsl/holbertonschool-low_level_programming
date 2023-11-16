6e31238e19fbb68485a4402fe74109aa84c9f439
Alfredolsl <alfredo.445566@gmail.com>
2023-11-15 18:12:02 -0800

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int _atoi(char *s);
+
+/**
+ * main - add positive numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else
+	{
+		int i;
+		int j = 0;
+
+		for (i = 1; i < argc; i++)
+		{
+			if !((argv[i] > 'a' && argv[i] < 'z') || (argv[i] > 'A' && argv[i] < 'Z'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				j += _atoi(i);
+			}
+		}
+		printf("%d\n", j);
+	}
+	return (0);
+}
