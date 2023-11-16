ec26db00d53e789a93a1424577331da67fd43729
Alfredolsl <alfredo.445566@gmail.com>
2023-11-15 16:22:58 -0800

    whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints a program's name
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+This repository contains projects relating to arg count and arg vector in a function input
