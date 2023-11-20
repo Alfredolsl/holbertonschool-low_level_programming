835dca8787cab17f59e251f3dc101758c48a9425
Alfredolsl <alfredo.445566@gmail.com>
2023-11-19 17:16:36 -0800

    str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,25 +12,26 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len, concat;
+	int i, j, len1, len2, concat;
 	char *s3;
 
-	len = 0;
+	len1 = 0;
+	len2 = 0;
 
 	if (s1 == NULL)
 		i = 0;
 	else
 	{
-		while (s1[len])
-			len++;
+		while (s1[len1])
+			len1++;
 	}
 
 	if (s2 == NULL)
 		j = 0;
 	else
 	{
-		while (s2[len])
-			len++;
+		while (s2[len2])
+			len2++;
 	}
 
 	s3 = malloc(sizeof(char) * len + 1);
