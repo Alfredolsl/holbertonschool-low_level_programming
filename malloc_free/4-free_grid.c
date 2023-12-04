f93367945b2fb7bef5a466463ebb45eef2d7110b
Alfredolsl <alfredo.445566@gmail.com>
2023-12-04 12:27:01 -0800

    frees a 2d grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2d grid created by alloc_grid
+ * @grid: address of 2d grid
+ * @height: height of grid
+ */
+
+void free_grid
+{
+	free(grid);
+}
