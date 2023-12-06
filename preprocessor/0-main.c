775af4ac4276459a2b1e0500d401be1d92419585
Alfredolsl <alfredo.445566@gmail.com>
2023-12-05 19:44:29 -0800

    header file that defines a macro named SIZE as abbreviation for token 1024

---
+++ preprocessor/0-main.c
@@ -0,0 +1,14 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - checks code
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	printf("%d\n", 98 + SIZE);
+	return (0);
+}
