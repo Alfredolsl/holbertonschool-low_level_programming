cd834b7decfcbbfd4aefb8028a637839437ec8ef
Alfredolsl <alfredo.445566@gmail.com>
2023-12-06 16:19:07 -0800

    poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,6 +8,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - poppy
+ * @name: char pointing to variable called owner
+ * @age: float type
+ * @owner: char pointing to variable called owner
+ *
+ * Description: poppy
+ */
+
 struct dog
 {
 	char *name;
