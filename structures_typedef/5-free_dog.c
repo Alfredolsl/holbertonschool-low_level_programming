578aab2967efd061c669ab8e98418c38895aa247
Alfredolsl <alfredo.445566@gmail.com>
2023-12-06 22:35:18 -0800

    free dog structure

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dog struct
+ * @d: dog struct to free
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
