86de4f13679771f93f09609a78c559ac55950d03
Alfredolsl <alfredo.445566@gmail.com>
2023-12-06 17:12:19 -0800

    prints a struct dogh

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_dog - print a struct dog
+ * @d: struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit();
+
+	printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
+	printf("Age: %.6lf\n", (d->age == NULL) ? "(nil)" : d->age);
+	printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
+}
