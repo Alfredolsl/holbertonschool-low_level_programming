e40f14b40fe6d6324f746648fe00c5a6a3de73cb
Alfredolsl <alfredo.445566@gmail.com>
2023-12-09 14:14:10 -0800

    function that prints anything using switch case

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format - format of the variable
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *str, *separator;
+	const char *current_format = format;
+
+	separator = "";
+	current_format = format;
+	va_start(args, format);
+
+	while (current_format != NULL && *current_format != '\0')
+	{
+		switch (*current_format)
+		{
+			case 'c':
+				printf("%s%c", separator va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%d", separator va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator va_arg(args, str));
+				break;
+		}
+		separator = ", ";
+		current_format++;
+	}
+	printf("\n");
+	va_end(args);
+}
