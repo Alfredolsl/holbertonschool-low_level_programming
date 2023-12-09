41a8dbf0d03e8c35a093c1ca1e17a8dfc2500f1b
Alfredolsl <alfredo.445566@gmail.com>
2023-12-08 20:37:47 -0800

    searches for an integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -3,11 +3,12 @@
 
 /**
  * int_index - function that searches an integer
- * @array: array to search integer
+ * @array: pointer to array
  * @size: size of array
  * @cmp: pointer to the function to be used to compare values
  *
- * Return: index of the first element for which the cmp function does not return 0,
+ * Return: index of the first element for which the 
+ * cmp function does not return 0,
  * if no element matches or size <= 0, return -1
  */
 
@@ -19,7 +20,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			(*cmp)(array[i]);
+			if ((*cmp)(array[i]) != 0)
+				return (i);
 		}
 	}
 	return (-1);
