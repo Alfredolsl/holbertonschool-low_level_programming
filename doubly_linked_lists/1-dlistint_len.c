20ecc29303419816ee45dd68ba40024ba66aa7af
Alfredolsl <alfredo.445566@gmail.com>
2023-12-13 11:19:46 -0800

    List length

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements
+ * in a linked dlistint_t list
+ * @h: first node
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+}
