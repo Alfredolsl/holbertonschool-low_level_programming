9ac869b7f611031c289ebf0f3ecc69893faa62a4
Alfredolsl <alfredo.445566@gmail.com>
2023-12-14 11:44:04 -0800

    0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int power = 0;
+	int power = 1;
 
 	while (*b) /* true if current char is not '\0' */
 	{
 		if (*b != '0' || *b != '1')
 			return (0);
 
-		result = (*b - '0') * (2**power);
-		power++;
+		result = (*b - '0') * (2*power);
+		power *= 2;
 		b++;
 	}
 
