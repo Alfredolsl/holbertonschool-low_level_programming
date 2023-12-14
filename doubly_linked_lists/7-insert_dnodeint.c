27217ff5d7021141b57c0810c8565899358fefe6
Alfredolsl <alfredo.445566@gmail.com>
2023-12-14 10:26:04 -0800

    Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -38,13 +38,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		counter++;
 	}
+	if (current == NULL)
+		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = current;
 	new_node->next = current->next;
 
-	if (current->next != NULL)
-		current->next->prev = new_node;
+	current->next->prev = new_node;
 	current->next = new_node;
 
 	return (new_node);
