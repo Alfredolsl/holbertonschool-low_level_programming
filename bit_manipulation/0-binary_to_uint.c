ccb8457a14c11b00dc582b9d3a3e1a49ba2be402
Alfredolsl <alfredo.445566@gmail.com>
2023-12-14 11:45:41 -0800

    0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 		if (*b != '0' || *b != '1')
 			return (0);
 
-		result = (*b - '0') * power;
+		result += (*b - '0') * power;
 		power *= 2;
 		b++;
 	}
