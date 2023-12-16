8e767fa8df8367696f283d4f4f951d58a820494b
Alfredolsl <alfredo.445566@gmail.com>
2023-12-16 14:02:09 -0800

    copy file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -38,16 +38,7 @@ int main(int argc, char *argv[])
 	fd_to = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
-	bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-
-	if (bytes_read == -1)
-	{
-		close_file(file_from, fd_from);
-		close_file(file_to, fd_to);
-		print_error_and_exit(98, "Error: Can't read from file %s\n", file_from);
-	}
-
-	while (bytes_read > 0)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
@@ -56,6 +47,13 @@ int main(int argc, char *argv[])
 			close_file(file_to, fd_to);
 			print_error_and_exit(99, "Error: Can't write to %s\n", file_to);
 		}
+	}
+
+	if (bytes_read == -1)
+	{
+		close_file(file_from, fd_from);
+		close_file(file_to, fd_to);
+		print_error_and_exit(98, "Error: Can't read from file %s\n", file_from);
 	}
 
 	close_file(file_from, fd_from);
