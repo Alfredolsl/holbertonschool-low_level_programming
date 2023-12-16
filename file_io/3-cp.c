234eba637edea4f8e608b7dba0620c79a50213f4
Alfredolsl <alfredo.445566@gmail.com>
2023-12-16 12:28:47 -0800

    copy file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,109 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#define BUFFER_SIZE 1024
+
+void print_error_and_exit(int error_code, const char *message, ...);
+
+int open_file(const char *filename, int flags, mode_t mode);
+
+void close_file(const char *filename, int fd);
+
+/**
+ * main - main function for copying one file to another
+ * @argc: argument countn
+ * @argv: argument vector
+ *
+ * Return: 0 if successful, otherwise exit with error code
+ */
+
+int main(int argc, char *argv[])
+{
+	const char *file_from = argv[1];
+	const char *file_to = argv[2];
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	fd_from = open(file_from, O_RDONLY);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while ((bytes_read = read(file_to, buffer, BUFFER_SIZE) > 0))
+	{
+		bytes_written = write(file_to, buffer, BUFFER_SIZE);
+		if (bytes_written == -1)
+		{
+			close_file(file_from, fd_from);
+			close_file(file_to, fd_to);
+			print_error_and_exit(99, "Error: Can't write to %s\n", file_to);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		close_file(file_from, fd_from);
+		close_file(file_to, fd_to);
+		print_error_and_exit(98, "Error: Can't read from file %s\n", file_from);
+	}
+
+	close_file(file_from, fd_from);
+	close_file(file_to, fd_to);
+	return (0);
+}
+
+/**
+ * print_error_and_exit - handles error and aborts function
+ * @error_code: error code encountered
+ * @message: error message to write on screen
+ */
+
+void print_error_and_exit(int error_code, const char *message, ...)
+{
+	va_list args;
+
+	va_start(args, message);
+	dprintf(STDERR_FILENO, message, args);
+	va_end(args);
+
+	exit(code);
+}
+
+/**
+ * open_file - opens a file
+ * @filename: name of file to open
+ * @flags: read, write, or do both to the file
+ * @mode: file permission bits
+ *
+ * Return: nonnegative integer, if an error is encountered, returns -1
+ */
+
+int open_file(const char *filename, int flags, mode_t mode)
+{
+	int fd = open(filename, flags, mode);
+
+	if (fd == -1)
+		print_error_and_exit(99, "Error: Can't write to %s\n", filename);
+
+	return (fd);
+}
+
+/**
+ * close_file - closes a file
+ * @filename: name of file to close
+ * @fd: returned number of open function
+ */
+
+void close_file(const char *filename, int fd)
+{
+	if (fd == -1)
+		print_error_and_exit(100, "Error: Can't close fd for file %s\n", filename)
+}
