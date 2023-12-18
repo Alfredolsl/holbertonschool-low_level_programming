c28d19e054cdd39a9446c1a94ea1d2d86847cea4
Alfredolsl <alfredo.445566@gmail.com>
2023-12-17 17:16:45 -0800

    prints a hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to be printed
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *node;
+	char *separator;
+
+	separator = "";
+
+	if (ht == NULL)
+		return;
+
+	putchar('{');
+
+	for (i = 0; ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			printf("%s", separator);
+
+			node = ht->array[i];
+
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+
+				if (node != NULL)
+					printf(", ");
+			}
+			separator = ", ";
+		}
+	}
+	putchar('}');
+}
