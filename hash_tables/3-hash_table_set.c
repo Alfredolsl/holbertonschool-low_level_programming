d3a21f22ba267503877dacd06e788cbca01e2be3
Alfredolsl <alfredo.445566@gmail.com>
2023-12-17 16:22:56 -0800

    adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,19 +30,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			ht->array[i]->value = copy_value;
 			return (1);
 		}
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
-		free(value_copy);
+		free(copy_value);
 		return (0);
 	}
 
 	new_node->key = strdup(key);
-	new_node->value = value;
+	new_node->value = copy_value;
 	ht->array[index] = new_node;
 
 	return (1);
