9ec5dd66b16c78caa23dd013ddc3529cf7903082
Alfredolsl <alfredo.445566@gmail.com>
2024-01-14 18:40:23 -0800

    Linear Search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of ints
+ * using the Linear search algorithm
+ * @array: pointer to base address of array
+ * @size: size of array
+ * @value: value to search
+ *
+ * Return: index where value is located,
+ * if value is not present or if array is NULL, return NULL.
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+	if (!array || !*array)
+		return (-1);
+
+	for (i = 0; array[i] != '\0'; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
